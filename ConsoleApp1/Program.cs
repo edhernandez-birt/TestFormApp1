8c4d290ae25cc1b1c74b98f50578103cda4eb1da
Eduardo Hernandez <edhernandez@birt.eus>
2022-02-01 11:19:20 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using BiblioDLL;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"\\Historial.txt");
+            Class1 clase = new();
+        }
+    }
+}
